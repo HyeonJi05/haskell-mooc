5a0ed4809b810a4d907924619792f736a9c06a4d
HyeonJi05 <eockdwndbth@naver.com>
2023-12-01 14:19:45 +0900

    Set3a.hs ex2

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -42,7 +42,9 @@ maxBy measure x y
 --   mapMaybe length (Just "abc") ==> Just 3
 
 mapMaybe :: (a -> b) -> Maybe a -> Maybe b
-mapMaybe f x = todo
+mapMaybe _ Nothing = Nothing
+mapMaybe f (Just x) = Just (f x)
+
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the function mapMaybe2 that works like mapMaybe
